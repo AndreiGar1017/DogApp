62778b73fcc059e7f5e1cfe3d856984ea5194be4
Andrei García <andreigar1017@gmail.com>
2024-02-21 22:29:32 -0600

    added local storage

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
 export default App = () => {
   const [imageData, setImageData] = useState(null);
+  const [localImage, setLocalImage] = useState(null);
   const [error, setError] = useState(null);
 
   const getImage = async() => {
@@ -13,21 +16,97 @@ export default App = () => {
       const data = await res.json();
       setImageData(data);
       setError(null);
-      console.log(imageData.message)
     }catch(err){
       setError(`Error finding dog Image!: ${err}`)
     }
   }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+
+    if (!result.canceled) {
+      storeData(result.assets[0].uri);
+    }
+  };
+
+  const storeData = async (value) => {
+    try {
+      const counter = await AsyncStorage.getItem('counter');
+      let newCounter = 0;
+  
+      if (counter !== null) {
+        newCounter = parseInt(counter) + 1;
+      }
+  
+      await AsyncStorage.setItem(`Img${newCounter}`, value);
+      await AsyncStorage.setItem('counter', newCounter.toString());
+
+      if (counter !== null) {
+        lastCounter = parseInt(counter) + 1
+      }else{
+        lastCounter = 0
+      }
+      const newImage = await AsyncStorage.getItem(`Img${lastCounter}`)
+      setLocalImage(newImage);
+      
+    } catch (e) {
+      console.error('Error on saving dog image!:', e);
+    }
+  };
+
+  const randomImage = async() =>{
+    const counter = await AsyncStorage.getItem('counter');
+    console.log("counter: ",counter)
+    let randomIndex = Math.floor(Math.random() * (parseInt(counter)+1));
+    console.log("index: ",randomIndex)
+    const imageRandom = await AsyncStorage.getItem(`Img${randomIndex}`)
+    setLocalImage(imageRandom);
+  }
+
+  const restartAsyncStorage = async () => {
+    try {
+      await AsyncStorage.clear();
+      console.log('AsyncStorage has been restarted');
+    } catch (error) {
+      console.error('Error on restarting AsyncStorage', error);
+    }
+  };
+
+  const localOnInit = async() =>{
+    const counter = await AsyncStorage.getItem('counter');
+    if(counter>0){
+      randomImage();
+    }
+  }
+
+  useEffect(() => {
+    getImage();
+    localOnInit();
+    // restartAsyncStorage();
+  }, []); 
 
 
   return[
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to DogApp!</Text>
       <TouchableOpacity onPress={getImage}>
-        <Text>Get Image</Text>
+        <Text>Random Dog</Text>
       </TouchableOpacity>
       {error && (<Text>{error}</Text>)}
-      {imageData && (<Image style={styles.image} source={{uri:imageData.message}}/>)}
+      {imageData && (<Image style={styles.imageAPI} source={{uri:imageData.message}}/>)}
+      <TouchableOpacity onPress={pickImage}>
+        <Text>Pick an image from camera roll</Text>
+      </TouchableOpacity>
+      {localImage && <Image style={styles.imageAPI} source={{ uri: localImage }} />}
+      {localImage &&  <TouchableOpacity onPress={randomImage}>
+                        <Text>Randomize Local Images</Text>
+                      </TouchableOpacity>}
     </View>
   ]
 }
@@ -43,7 +122,7 @@ const styles = StyleSheet.create({
     marginBottom:20,
     fontWeight: "bold"
   },
-  image:{
+  imageAPI:{
     width: 200,
     height: 180,
     margin: 10
