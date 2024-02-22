060d768e1dcae48d83cf21397802f81552657348
AndreiGar1017 <andreigar1017@gmail.com>
2024-02-21 18:02:34 -0600

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React, {useState, useEffect} from 'react'
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native'
+
+
 
 export default App = () => {
-  <View style={styles.container}>
-    <Text style={styles.title}>Welcome to DogApp!</Text>
-  </View>
+  const [image, setImage] = useState(null);
+  const [error, setError] = useState(null);
+
+  const getImage = async() => {
+    try{
+      const res = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await res.json();
+      setImage(data);
+      setError(null);
+    }catch(err){
+      setError(`Error finding weather data!`)
+    }
+  }
+
+  useEffect(()=>{
+    getImage();
+  },[]);
+
+  return[
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to DogApp!</Text>
+      <TouchableOpacity onPress={getImage}>
+        <Text>Get Image</Text>
+      </TouchableOpacity>
+      {error && <Text>{error}</Text>}
+      {image && <Image source={{uri: image.message}}/>}
+
+    </View>
+  ]
 }
 
 const styles = StyleSheet.create({
